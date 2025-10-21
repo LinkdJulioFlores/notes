0cb9a75fb3fdb8e14a47afee8382e9fd442b0497
Julio Flores <nexus@Julios-MacBook-Pro.local>
2025-10-20 20:48:39 -0400

    Added a challenge in webhookService.ts

--- src/server/webhookService.ts
+++ src/server/webhookService.ts
@@ -1,18 +1,21 @@
 import type { EventType } from "@prisma/client";
 
+// Challenge: See if you can document this and fully grasp what's going
+// on here?
+
 export class WebhookService {
   static async updateWebhook<T>(url: string, type: EventType, data: T) {
     const urlObj = new URL(url);
 
     try {
-      const res = await fetch(urlObj.toString(), {
+      const response = await fetch(urlObj.toString(), {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ type, timestamp: Date.now(), data }),
       });
-      if (!res.ok) {
+      if (!response.ok) {
         console.error(
-          `Webhook request failed: ${res.status} ${res.statusText}`,
+          `Webhook request failed: ${response.status} ${response.statusText}`,
         );
       }
     } catch (err) {
